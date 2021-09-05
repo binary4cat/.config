3234079ce05c374569a4e836a74be54d20b158c8
_Is2 <haijiandong@outlook.com>
2021-09-05 17:01:35 +0800

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,8 @@
+# first install zsh and before install the zimfw
+pacman -S zsh
+wget -nv -O - https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 # get my .config
 git clone https://github.com/hjdo/.config.git $HOME/.config
 # link zshrc config file
 ln -s $HOME/.config/zsh/zshrc $HOME/.zshrc
+ln -s $HOME/.config/zsh/zimrc $HOME/.zimrc
