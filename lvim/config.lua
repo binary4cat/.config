584f9128fe1cb82230862f8d610051125ba3270c
binary4cat <haijiandong@outlook.com>
2021-11-27 13:54:09 +0800

    Modify the hop.vim key bind.

--- lvim/config.lua
+++ lvim/config.lua
@@ -145,8 +145,10 @@ lvim.plugins = {
     event = "BufRead",
     config = function()
       require("hop").setup()
-      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "s", ":HopWord<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "m", ":HopChar1<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "L", ":HopLine<cr>", { silent = true })
     end,
   },
   -- {
