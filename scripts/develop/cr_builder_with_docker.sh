6c32869543fad61da98fa749ca0a7baf6cba985f
binary4cat <haijiandong@outlook.com>
2021-12-14 18:32:33 +0800

    Modify the dev docker script.

--- scripts/develop/cr_builder_with_docker.sh
+++ scripts/develop/cr_builder_with_docker.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
 
-echo 'FROM $1
-RUN apt-get update && apt-get -y install sshfs openssl
-RUN mkdir /mnt/data
-RUN echo "#!/bin/bash\n\
-  sshfs binary4cat@127.0.0.1:$2/ /mnt/data -o auto_cache,reconnect,transform_symlinks,follow_symlinks\n\
-  bash\n" > start.sh
-ENTRYPOINT [ "/bin/bash", "start.sh" ]' > /tmp/bulder_fockerfile 
+GOPATH=$(go env GOPATH)
+GOPROXY=$(go env GOPROXY)
+GOPRIVATE=$(go env GOPRIVATE)
 
-docker build -t builder -f /tmp/bulder_fockerfile .
+build_docker(){
+  echo '
+  FROM ubuntu:18.04
+  RUN echo "deb http://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse\n\
+    deb http://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse\n\
+    deb http://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse\n\
+    deb http://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" > /etc/apt/sources.list
+  RUN apt-get update && apt-get -y install sshfs openssl curl upx git zsh gcc g++
+  RUN curl -L https://raw.githubusercontent.com/binary4cat/golang-tools-install-script/feature/mirror/goinstall.sh | bash -s -- --mirror https://mirrors.ustc.edu.cn/golang
+  # 自动安装的go会写入全局变量，这里需要修改文件
+  RUN sed -i "s/^\(export GOPATH=\).*$/\1'${GOPATH//\//\\\/}'/" ~/.bashrc
+  ENV GOPROXY='$GOPROXY'
+  ENV GOPRIVATE='$GOPRIVATE'
+  ENTRYPOINT [ "bash" ]' > /tmp/bulder_fockerfile 
+
+  docker build -t dev_builder -f /tmp/bulder_fockerfile .
+}
+
+run_docker(){
+  docker run\
+    -v $GOROOT:$GOROOT\
+    -v $GOPATH:$GOPATH\
+    -v /data:/mnt/data\
+    --net=host\
+    --privileged\
+    --cap-add SYS_ADMIN\
+    -it dev_builder
+}
+
+case "$1" in 
+  build)
+    build_docker
+    ;;
+  run)
+    run_docker
+    ;;
+  *)
+    echo "build | run"
+    ;;
+esac
