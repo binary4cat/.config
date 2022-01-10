a4fb6bc844ae7cecaa38d22d92dba36a5c43f0fb
binary4cat <haijiandong@outlook.com>
2022-01-10 20:51:33 +0800

    Set transparent for nvim.

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,10 +1,29 @@
-local colorscheme = "tokyonight"
+local _COLORSCHEME_ = "tokyonight"
+local _TRANSPARENT_ = true
 
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_lualine_bold = true
+local function enable_transparent_mode()
+  vim.cmd "au ColorScheme * hi Normal ctermbg=none guibg=none"
+  vim.cmd "au ColorScheme * hi SignColumn ctermbg=none guibg=none"
+  vim.cmd "au ColorScheme * hi NormalNC ctermbg=none guibg=none"
+  vim.cmd "au ColorScheme * hi MsgArea ctermbg=none guibg=none"
+  vim.cmd "au ColorScheme * hi TelescopeBorder ctermbg=none guibg=none"
+  vim.cmd "au ColorScheme * hi NvimTreeNormal ctermbg=none guibg=none"
+  vim.cmd "au ColorScheme * hi EndOfBuffer ctermbg=none guibg=none"
+  vim.cmd "let &fcs='eob: '"
+end
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if ( _TRANSPARENT_ ) then
+  enable_transparent_mode()
+end
+
+if ( _COLORSCHEME_=="tokyonight" ) then
+  vim.g.tokyonight_style = "night"
+  vim.g.tokyonight_lualine_bold = true
+end
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. _COLORSCHEME_)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  vim.notify("colorscheme " .. _COLORSCHEME_ .. " not found!")
   return
 end
+
