c4110bbcfffeb15fd4499b3ce9d7bf1c71dc17b4
binary4cat <haijiandong@outlook.com>
2022-01-12 12:26:40 +0800

    Modify fish config.

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,31 +1,31 @@
-set -U PATH            $HOME/.local/bin:$PATH
-set -U XDG_CONFIG_HOME $HOME/.config
-set -U LOCALBIN        $XDG_CONFIG_HOME/bin
-set -U PATH            $PATH:$LOCALBIN
-set -U LOCALPROG       $HOME/prog
+set -x PATH            $HOME/.local/bin:$PATH
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x LOCALBIN        $XDG_CONFIG_HOME/bin
+set -x PATH            $PATH:$LOCALBIN
+set -x LOCALPROG       $HOME/prog
 
 # golang
-set -U GO111MODULE on
-set -U GOPROXY     "https://goproxy.cn,direct"
-set -U GOPRIVATE   "devgit.starschina.com/*"
-set -U PATH        $PATH:/usr/local/bin 
-set -U PATH        $PATH:$HOME/go/bin
-set -U PATH        "$PATH":"$HOME/.pub-cache/bin"
+set -x GO111MODULE on
+set -x GOPROXY     "https://goproxy.cn,direct"
+set -x GOPRIVATE   "devgit.starschina.com/*"
+set -x PATH        $PATH:/usr/local/bin 
+set -x PATH        $PATH:$HOME/go/bin
+set -x PATH        "$PATH":"$HOME/.pub-cache/bin"
 
 #export TERM=xterm-256color
 #export TERM_ITALICS=true
-set -U RANGER_LOAD_DEFAULT_RC        "false"
-set -U EDITOR                        nvim
-set -U ZSH_AUTOSUGGEST_USE_ASYNC     1
-set -U ZSH_AUTOSUGGEST_MANUAL_REBIND 1
-set -U POWERLEVEL9K_CONFIG_FILE      $HOME/.config/.p10k.zsh
-set -U ZIM_HOME                      $HOME/.zim
-set -U STARSHIP_CONFIG               ~/.config/zsh/starship/config.toml
+set -x RANGER_LOAD_DEFAULT_RC        "false"
+set -x EDITOR                        nvim
+set -x ZSH_AUTOSUGGEST_USE_ASYNC     1
+set -x ZSH_AUTOSUGGEST_MANUAL_REBIND 1
+set -x POWERLEVEL9K_CONFIG_FILE      $HOME/.config/.p10k.zsh
+set -x ZIM_HOME                      $HOME/.zim
+set -x STARSHIP_CONFIG               ~/.config/zsh/starship/config.toml
 
 # fcitx5
-set -U GTK_IM_MODULE  DEFAULT=fcitx
-set -U QT_IM_MODULE   DEFAULT=fcitx
-set -U XMODIFIERS     DEFAULT=@im=fcitx
-set -U INPUT_METHOD   DEFAULT=fcitx
-set -U SDL_IM_MODULE  DEFAULT=fcitx
-set -U GLFW_IM_MODULE DEFAULT=ibus
+set -x GTK_IM_MODULE  DEFAULT=fcitx
+set -x QT_IM_MODULE   DEFAULT=fcitx
+set -x XMODIFIERS     DEFAULT=@im=fcitx
+set -x INPUT_METHOD   DEFAULT=fcitx
+set -x SDL_IM_MODULE  DEFAULT=fcitx
+set -x GLFW_IM_MODULE DEFAULT=ibus
