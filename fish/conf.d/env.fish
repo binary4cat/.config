1754367b44a60480956f882094b291bb5b6ee00d
binary4cat <haijiandong@outlook.com>
2022-01-13 15:02:13 +0800

    Remove fcitx5 env from fish.

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -14,6 +14,9 @@ set GO111MODULE on
 set GOPROXY     "https://goproxy.cn,direct"
 set GOPRIVATE   "devgit.starschina.com/*"
 
+## npm
+set NPM_CONFIG_PREFIX  ~/.npm-global
+
 #export TERM=xterm-256color
 #export TERM_ITALICS=true
 set RANGER_LOAD_DEFAULT_RC        "false"
@@ -25,9 +28,9 @@ set ZIM_HOME                      $HOME/.zim
 set STARSHIP_CONFIG               ~/.config/zsh/starship/config.toml
 
 # fcitx5
-set GTK_IM_MODULE  DEFAULT=fcitx
-set QT_IM_MODULE   DEFAULT=fcitx
-set XMODIFIERS     DEFAULT=@im=fcitx
-set INPUT_METHOD   DEFAULT=fcitx
-set SDL_IM_MODULE  DEFAULT=fcitx
-set GLFW_IM_MODULE DEFAULT=ibus
+# set GTK_IM_MODULE  DEFAULT=fcitx
+# set QT_IM_MODULE   DEFAULT=fcitx
+# set XMODIFIERS     DEFAULT=@im=fcitx
+# set INPUT_METHOD   DEFAULT=fcitx
+# set SDL_IM_MODULE  DEFAULT=fcitx
+# set GLFW_IM_MODULE DEFAULT=ibus
