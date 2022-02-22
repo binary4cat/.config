c1318eb6fb14faac8a533031515472a50e412d56
binary4cat <haijiandong@outlook.com>
2022-02-22 10:27:52 +0800

    Modify vim gui font size.

--- nvim/lua/user/gui-config.lua
+++ nvim/lua/user/gui-config.lua
@@ -1,8 +1,8 @@
 -- gui
-vim.o.guifont = "JetBrainsMonoMedium NF"
+vim.o.guifont = "JetBrainsMonoMedium NF:h11"
 
 -- neovide
-vim.g.neovide_transparency = 0.8
+-- vim.g.neovide_transparency = 0.8
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_antialiasing = true
 vim.g.neovide_cursor_vfx_mode = "railgun"
