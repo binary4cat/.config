a8b6d5f1a5874f1a922810de3d9cc77a959dafa5
binary4cat <haijiandong@outlook.com>
2022-02-23 22:36:18 +0800

    Modify gui config。

--- nvim/lua/user/gui-config.lua
+++ nvim/lua/user/gui-config.lua
@@ -5,4 +5,4 @@ vim.o.guifont = "JetBrainsMonoMedium NF:h11"
 -- vim.g.neovide_transparency = 0.8
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_antialiasing = true
-vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
