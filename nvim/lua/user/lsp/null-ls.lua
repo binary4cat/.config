b1485cd649b9b6623a6418195d602f5de79b369f
binary4cat <haijiandong@outlook.com>
2022-05-07 22:30:47 +0800

    Fix nvim config errors.

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -7,49 +7,24 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- Async formatting
-_G.formatting = function(bufnr)
-	bufnr = tonumber(bufnr) or vim.api.nvim_get_current_buf()
-
-	vim.lsp.buf_request(
-		bufnr,
-		"textDocument/formatting",
-		{ textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
-		function(err, res)
-			if err then
-				local err_msg = type(err) == "string" and err or err.message
-				-- you can modify the log message / level (or ignore it completely)
-				vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-				return
-			end
-
-			-- don't apply results if buffer is unloaded or has been modified
-			if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-				return
-			end
-
-			if res then
-				vim.lsp.util.apply_text_edits(res, bufnr)
-				vim.api.nvim_buf_call(bufnr, function()
-					vim.cmd("silent noautocmd update")
-				end)
-			end
-		end
-	)
-end
 
 null_ls.setup({
 	debug = false,
-	on_attach = function(client)
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
-			-- wrap in an augroup to prevent duplicate autocmds
-			vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePost <buffer> lua formatting(vim.fn.expand("<abuf>"))
-            augroup END
-            ]])
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
 		end
 	end,
 	-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
