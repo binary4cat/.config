5d1b1cf2635c0ffa82a4e1684f5c47c9a57e07ec
binary4cat <haijiandong@outlook.com>
2023-05-09 12:05:57 +0800

    增加测试插件

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -384,4 +384,42 @@ return {
     event = { "CmdlineEnter" },
     build = "bash ./install.sh",
   },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-neotest/neotest-go",
+      "rouge8/neotest-rust",
+    },
+    event = { "CmdlineEnter" },
+    config = function()
+      -- get neotest namespace (api call creates or returns namespace)
+      local neotest_ns = vim.api.nvim_create_namespace("neotest")
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+      require("neotest").setup({
+        -- your neotest config here
+        adapters = {
+          require("neotest-go")({
+            experimental = {
+              test_table = true,
+            },
+            args = { "-count=1", "-timeout=60s", "-test.v" },
+          }),
+          require("neotest-rust")({
+            args = { "--no-capture" },
+            dap_adapter = "lldb",
+          }),
+        },
+      })
+    end,
+  },
 }
