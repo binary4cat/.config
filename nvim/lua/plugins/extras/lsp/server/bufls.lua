65a313602666d3ef0c2bfaa4c75d917e492abc5d
binary4cat <haijiandong@outlook.com>
2024-12-25 21:31:09 +0800

    增加新的vim插件

--- nvim/lua/plugins/extras/lsp/server/bufls.lua
+++ nvim/lua/plugins/extras/lsp/server/bufls.lua
@@ -1,6 +1,6 @@
 local lspconfig = require("lspconfig")
 return {
-  cmd = { "bufls", "serve" },
+  cmd = { "buf_ls", "serve" },
   filetypes = { "proto" },
   root_dir = lspconfig.util.root_pattern("buf.work.yaml", ".git"),
 }
